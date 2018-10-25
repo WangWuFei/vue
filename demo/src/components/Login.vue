0b29a7657b951cd84948ea55ae7ad981bef27f32
WangWuFei <1254419135@qq.com>
2018-10-25 17:54:48 +0800

    wf

---
+++ demo/src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="login" >
+    <div id="window" class="window-size">
+      ffffff
+    </div>
+    <img src="../image/login.jpg" style="height: 100%;width: 100%"/>
+  </div>
+</template>
+
+<script>
+import '../css/login/login.css'
+
+export default {
+  name: 'Login',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style>
+  #login {
+    height: 100%;
+    overflow: hidden;
+    position: relative;
+  }
+</style>
---
+++ demo/src/components/ThankCCTV.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button id="add" @click="addName" >anniu</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Thankcctv',
+  data () {
+    return {
+      ddd: {}
+    }
+  },
+  methods: {
+    addName: function () {
+      // document.getElementById('add').innerHTML = 'dddd'
+      this.$router.push('login')
+      // var element = this
+      // axios.get('http://119.23.63.32:8080/springnew/TestController/test.do', {
+      // })
+      //   .then(function (response) {
+      //     element.ddd = response
+      //     alert(response)
+      //   })
+      //   .catch(function (error) {
+      //     console.log(error)
+      //   })
+    }
+  }
+}
+</script>
